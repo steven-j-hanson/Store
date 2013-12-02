d34d23adb8fb6d5c8a0cf514928f9690b482ae56
Steven Hanson <steven_j_hanson@hotmail.com>
2013-12-01 18:17:29 -0600

    Added a category controller, added products, and added a category to the products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131201223708) do
+ActiveRecord::Schema.define(version: 20131201232209) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 20131201223708) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image_filename"
+    t.integer  "category_id"
   end
 
   create_table "provinces", force: true do |t|
